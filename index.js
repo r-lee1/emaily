2973de60df6895a6f9fcc6ce26b9c9b186eda634
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-04-19 13:55:43 -0700

    add google strategy config for passport

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
 
 //listen for port provided by heroku or default to 5000 in development
 const PORT = process.env.PORT || 5000;
