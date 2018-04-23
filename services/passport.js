5bc5f1dac47c5d1b829ff8b3606b0f340769878a
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-04-22 22:02:46 -0700

    add cookie session

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,15 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id})
-        .then((existingUser) => {
-          if (existingUser) {
-            //already have a record with given profile ID
-          } else {
-            //no user record with this ID, make new record
-            new User({ googleId: profile.id }).save();
-          }
-        });
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //already have a record with given profile ID
+          done(null, existingUser);
+        } else {
+          //no user record with this ID, make new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
