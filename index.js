42aa4522e1a30b39e5a2e1f52f84c8b7f23e820c
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-05-15 17:56:52 -0700

    add charge creation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     //30 days
@@ -21,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 //listen for port provided by heroku or default to 5000 in development
 const PORT = process.env.PORT || 5000;
