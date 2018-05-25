36a72dc4b8df28fadb34867247bae34d42797506
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-05-24 21:35:40 -0700

    add survey form review

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -32,7 +32,7 @@ class SurveyForm extends Component {
   render() {
     return(
       <div>
-      <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+      <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
         {this.renderFields()}
         <Link to="/surveys" className="red btn-flat white-text">
           Cancel
@@ -51,7 +51,7 @@ function validate(values) {
   const errors = {};
 
   errors.emails = validateEmails(values.emails || "");
-  
+
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
       errors[name] = "You must provide a value";
