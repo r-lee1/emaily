449ad35f8dedb5b2ef1895474ce014af6c84eef3
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-05-24 22:33:58 -0700

    add submit survey action

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
             to={this.props.auth ? '/surveys' : '/'}
             className="left brand-logo"
             >
-            MERNApp</Link>
+            Emaily</Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
